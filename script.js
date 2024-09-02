da61e43de4219fc6e6151dde34c6932464d04bb4
Soniprajapatisp <sonisp@proton.me>
2024-09-02 16:47:09 +0000

    primitive datatypes

---
+++ script.js
@@ -0,0 +1,14 @@
+// primitives data types (nn, bb, ss, u)
+alert("Hi")
+
+let a = null;          // null value
+let b = 123;           // number
+let c = true;          // boolean
+let d = BigInt("567"); // bigint
+let e = "Soni";        // string
+let f = Symbol("This is a symbol");  //  symbol
+let g  = undefined;     // undefine
+console.log(a,b,c,d,e,f,g);
+console.log(typeof d); 
+console.log(typeof c);
+
